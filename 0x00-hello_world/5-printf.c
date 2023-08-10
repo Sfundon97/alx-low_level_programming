68ac50eaa84f0c8fd962bda2207eb22bf07eb5c0
Sfundo Nondwatyu <sfundon97@gmail.com>
2023-08-10 13:15:56 -0700

    Programming in C!

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - this program prints a text
+ * Return: 0 (Means success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+		return (0);
+}

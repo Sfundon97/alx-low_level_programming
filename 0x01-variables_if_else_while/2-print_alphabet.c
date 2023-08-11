779c851e9213afa9615e433ab37b520602ec0e87
Sfundo Nondwatyu <sfundon97@gmail.com>
2023-08-11 16:18:43 -0700

    C variables

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - check the last digit
+ * Return: 0
+ */
+int main(void)
+{
+	int n = 97;
+	while (nn <= 122)
+	{
+		putchar(n);
+		n++
+	}
+	putchar('\n')
+	return (0);
+}

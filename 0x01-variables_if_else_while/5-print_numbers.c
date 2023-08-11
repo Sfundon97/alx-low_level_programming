7314169724992e9bc1b579fefa66aabb126851c4
Sfundo Nondwatyu <sfundon97@gmail.com>
2023-08-11 16:42:03 -0700

    C variables

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - this is an entry point
+ * Return: 0
+ */
+int main(void)
+{
+	itn i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n"0;
+	return (0);
+}

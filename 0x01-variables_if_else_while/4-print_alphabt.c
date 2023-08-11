9b751e01dbbf737fb695b5d88af27ee36e3cd5c9
Sfundo Nondwatyu <sfundon97@gmail.com>
2023-08-11 16:35:08 -0700

    C variables

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - check the last digit
+ * Return: 0
+ */
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		if (n == 101 || n == 113)
+		{
+			n++;
+			continue;
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}

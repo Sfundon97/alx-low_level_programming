53b793dfa9adc2557da0728a1707c5b6a555bf54
Sfundo Nondwatyu <sfundon97@gmail.com>
2023-08-11 17:22:35 -0700

    C variables

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,11 +8,11 @@ int main(void)
 	int n;
 	int c;
 
-	for (n = 48; n < 58; n++)
+	for (n = 48; n <= 57; n++)
 	{
 		putchar(n);
 	}
-	for (c = 97; c < 103; c++)
+	for (c = 97; c <= 102; c++)
 	{
 		putchar(c);
 	}

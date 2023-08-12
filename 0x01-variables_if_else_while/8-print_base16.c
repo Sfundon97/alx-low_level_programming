0f3ea13ad34e1691c03f8225a8edb667241b8a6a
Sfundo Nondwatyu <sfundon97@gmail.com>
2023-08-11 17:12:14 -0700

    C variables

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - print numbers with abase sixteen
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+	int c;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	for (m = 97; n < 103; m++)
+	{
+		putchar(m);
+	}
+	putchar('\n');
+return (0);
+}

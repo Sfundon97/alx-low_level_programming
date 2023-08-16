bf25a911da859a6cf1795a728d8bbfadee9125b3
Sfundo Nondwatyu <sfundon97@gmail.com>
2023-08-16 14:46:08 -0700

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _abs - Compute an integer
+ * @n - number to be computed
+ * Return: number or zero
+ */
+int _abs(int n)
+{
+	int abs_value;
+
+	if (n > 0)
+	{
+		abs_value = n * -1;
+		return (abs_value);
+	}
+	else
+	{
+		return (n);
+	}
+}

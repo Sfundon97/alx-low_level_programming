d0ebac7cbdbe8fef7f8fd3710072d257d159e55f
Sfundo Nondwatyu <sfundon97@gmail.com>
2023-08-16 16:08:57 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -27,16 +27,16 @@ void print_to_98(int n)
 	{
 	for (; n >= 98; n--)
 	{
-		if (n == 98)
-                {
-                        printf("%d", n);
-                        printf("\n");
-                        break;
-                }
-                else
-                {
-                        printf("%d, ", n);
-		}
+	if (n == 98)
+	{
+	printf("%d", n);
+	printf("\n");
+	break;
+	}
+	else
+	{
+	printf("%d, ", n);
+	}
 	}
 	}
 }

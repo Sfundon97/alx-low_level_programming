3698d976611fd34f7ead09048f14abc28abae8a3
Sfundo Nondwatyu <sfundon97@gmail.com>
2023-08-18 17:12:32 -0700

    0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper - check for uppercase
+ *  @c: the number to be checked
+ *  Return: 1 if it is uppercase or 0 if not
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && <= 98)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}

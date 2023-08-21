8fa241b4a9bffc6027c438f9129770c8343cadd4
Sfundo Nondwatyu <sfundon97@gmail.com>
2023-08-21 15:44:20 -0700

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * puts_half - print the second half of the string
+ * @str: string to be printed
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int j = 0, i;
+
+	while (str[j] != '\n')
+	{
+		j++;
+	}
+	if (j % 2 == 1)
+	{
+	i = (j - 1) / 2;
+	i += 1;
+	}
+	else
+	{
+		i = j / 2;
+	}
+	for (; i < j; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}

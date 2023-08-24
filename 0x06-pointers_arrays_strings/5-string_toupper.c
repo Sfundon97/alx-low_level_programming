b39372477396375faf6757b83f88c2441969b1d1
Sfundo Nondwatyu <sfundon97@gmail.com>
2023-08-24 15:15:39 -0700

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -9,12 +9,11 @@ char *string_toupper(char *s)
 {
 	int i = 0;
 
-	while (s[i] != '\0')
-	{
-		if (s[i] >= 'a' && s[i] <= 'z')
-
-			s[i] = s[i] - 32;
-			i++;
-	}
-	return (s);
+while (s[i] != '\0')
+{
+if (s[i] >= 'a' && s[i] <= 'z')
+s[i] = s[i] - 32;
+i++;
+}
+return (s);
 }

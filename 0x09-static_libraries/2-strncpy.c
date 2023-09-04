0f29cbc9a5511a2a4a6ff34f720b79361ef51e3b
Sfundo Nondwatyu <sfundon97@gmail.com>
2023-09-04 16:55:40 -0700

    0x09-static_libraries

---
+++ 0x09-static_libraries/2-strncpy.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+* _strncpy - copy a string
+* @dest: input 1
+* @src: input 2
+* @n: input 3
+* Return: destination
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+int j;
+
+j = 0;
+while (j < n && src[j] != '\0')
+{
+dest[j] = src[j];
+j++;
+}
+while (j < n)
+{
+dest[j] = '\0';
+j++;
+}
+
+return (dest);
+}
+
+
